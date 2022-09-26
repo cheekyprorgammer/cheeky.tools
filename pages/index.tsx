b3e66291e0843e405f48170021ee88e7faf472a6
cheekyprogrammer <cheekyprogrammer@gmail.com>
2022-09-26 18:29:38 +0000

    fixed meta tags and viewport

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ const Home: NextPage = () => {
     <div className="">
       <Head>
         <title>Cheeky Tools</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="A bunch of web tools designed to help you live a cheeky life. Made by cheekyprogrammer." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
