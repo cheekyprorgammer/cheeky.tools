86b4b77457a20ace6df672a69ace3bd14a918fd8
cheekyprogrammer <cheekyprogrammer@gmail.com>
2022-09-27 23:05:05 +0000

    changed md5 ui

--- pages/tools/md5.tsx
+++ pages/tools/md5.tsx
@@ -31,7 +31,7 @@ const Page: NextPage = () => {
 
 <div className="mb-6">
     <label htmlFor="output" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your md5 Hash</label>
-    <div id="output" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+    <div id="output" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" />
   </div>
     </div>
   )
