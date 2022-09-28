56cdd2850e24392e0222f86992ed89c7fcbda8e6
cheekyprogrammer <cheekyprogrammer@gmail.com>
2022-09-28 14:12:30 +0000

    fixed md5 ui

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -4,7 +4,7 @@ let year = d.getFullYear();
 export default function Footer() {
     return (
       <footer>
-        <div className="mt-5 text-sm text-center">
+        <div className="my-10 text-sm text-center">
             Copyright {year} by <a href="https://github.com/cheekyprorgammer" target="_blank" rel="noopener noreferrer"><span className="text-blue-500">cheekyprogrammer</span></a>.
         </div>
       </footer>
