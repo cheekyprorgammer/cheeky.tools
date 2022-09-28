22c3d3a3c5777e41279406e62840edb7e22fd12a
cheekyprogrammer <cheekyprogrammer@gmail.com>
2022-09-28 19:12:31 +0000

    fixed privacy link

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -64,7 +64,7 @@ const Privacy: NextPage = () => {
 
     <h2 className="text-2xl font-medium">Google DoubleClick DART Cookie</h2>
 
-    <p>Google is one of a third-party vendor on our site. It also uses cookies, known as DART cookies, to serve ads to our site visitors based upon their visit to www.website.com and other sites on the internet. However, visitors may choose to decline the use of DART cookies by visiting the Google ad and content network Privacy Policy at the following URL – <a href="https://policies.google.com/technologies/ads">https://policies.google.com/technologies/ads</a></p>
+    <p>Google is one of a third-party vendor on our site. It also uses cookies, known as DART cookies, to serve ads to our site visitors based upon their visit to www.website.com and other sites on the internet. However, visitors may choose to decline the use of DART cookies by visiting the Google ad and content network Privacy Policy <a href="https://policies.google.com/technologies/ads">here</a>.</p>
 
     <h2 className="text-2xl font-medium">Advertising Partners Privacy Policies</h2>
 
