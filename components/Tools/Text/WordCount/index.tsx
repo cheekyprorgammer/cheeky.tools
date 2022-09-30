36b40ada3705745628a898e2216fc3f9901d4620
cheekyprogrammer <cheekyprogrammer@gmail.com>
2022-09-30 23:43:21 +0000

    added results to wordcount tool

--- components/Tools/Text/WordCount/index.tsx
+++ components/Tools/Text/WordCount/index.tsx
@@ -4,7 +4,12 @@ import { useState, useEffect } from 'react'
 
 export default function WordCount() {
   const [inputString, setInputString] = useState("")
-  // const [copy, setCopy] = useState(false)
+  const [words, setWords] = useState(0)
+  const [charsWithSpace, setCharsWithSpace] = useState(0)
+  const [spaces, setSpaces] = useState(0)
+  const [charsWithoutSpace, setCharsWithoutSpace] = useState(0)
+  const [syllables, setSyllables] = useState(0)
+  const [loading, setLoading] = useState(false)
 
   function pasteText() {
     let textArea: any = document.getElementById('message')
@@ -19,8 +24,61 @@ export default function WordCount() {
       
   }
 
+  function handleClick() {
+
+    function syllableCount(word: string) {
+      word = word.toLowerCase()
+      var t_some = 0
+      if(word.length>3)
+        {
+        if(word.substring(0,4)=="some")
+            {
+            word = word.replace("some","")
+            t_some++
+            }
+        }
+      word = word.replace(/(?:[^laeiouy]|ed|[^laeiouy]e)$/, '')   
+      word = word.replace(/^y/, '')                                
+      //return word.match(/[aeiouy]{1,2}/g).length  
+      var syl = word.match(/[aeiouy]{1,2}/g)
+      console.log(syl)
+      if(syl)
+        {
+            //console.log(syl)
+            return syl.length+t_some
+        }
+    }
+
+    const results = document.getElementById("results")
+
+    setLoading(true)
+
+    let words = inputString.split(/\S+/).length - 1
+    setWords(words)
+    // console.log("There are " + (inputString.split(/\S+/).length - 1) + " words.")
+
+    let charsWithSpace = inputString.length
+    setCharsWithSpace(charsWithSpace)
+    // console.log("There are " + inputString.length + " characters (with spaces).")
+
+    let spaces = inputString.split(" ").length - 1
+    setSpaces(spaces)
+    // console.log((inputString.split(" ").length -1) + " space(s)")
+
+    let charsWithoutSpace = charsWithSpace - spaces
+    setCharsWithoutSpace(charsWithoutSpace)
+
+    setLoading(false)
+    results!.style.display = "block"
+    results!.style.height = "100%"
+
+    let syllables: any = syllableCount(inputString)
+    setSyllables(syllables)
+
+  }
+
   useEffect(() => {
-    console.log("loaded page")
+    
   }, [])
 
   return <>
@@ -52,7 +110,7 @@ export default function WordCount() {
               </svg>
             </div>
 
-            <div onClick={() => setInputString('')} className={`text-red-500 ${inputString.length > 0 ? 'block' : 'hidden'} cursor-pointer`}>
+            <div onClick={() => {setInputString(''); document.getElementById("results")!.style.display = "none";}} className={`text-red-500 ${inputString.length > 0 ? 'block' : 'hidden'} cursor-pointer`}>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                 <path fillRule="evenodd" d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z" clipRule="evenodd" />
               </svg>
@@ -61,10 +119,18 @@ export default function WordCount() {
         </div>
 
         <textarea id="message" value={inputString} onChange={(e) => setInputString(e.target.value)} rows={16} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" ></textarea>
-        <div className={`${inputString.length >0 ? 'block' : 'hidden'} mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}>Calculate</div>
+        <div onClick={handleClick} className={`${inputString.length >0 ? 'block' : 'hidden'} mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}>Calculate</div>
       </div>
 
       {/* Results Displayed Here */}
+      <div id="results" className={`py-5 hidden h-0 transition-all duration-200`}>
+        <p className="font-bold underline">Results:</p>
+        <p>Words: {words}</p>
+        <p>Characters (including spaces): {charsWithSpace}</p>
+        <p>Characters (without spaces): {charsWithoutSpace}</p>
+        <p>Spaces: {spaces}</p>
+        <p>Syllables: {syllables}</p>
+      </div>
 
       <p className="text-sm"><span className="font-bold underline uppercase">Note:</span> All content generated by this tool is done client-side (on your local computer) using JavaScript. None of your data is recorded or stored by us at all. Please view our <span className="text-blue-500">Privacy Policy</span> for more information.</p>
     </div>
